85bf6494a4555b143e3eb604aa0a0849d61ce171
Eric Miller <eric@legoaces.org>
2021-04-05 19:17:56 -0700

    Script modifications 2021

--- linux-recover.sh
+++ linux-recover.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Uninstall pre-installed stuff
+sudo apt-get purge thunderbird rhythmbox
+
 # scripts
 sudo cp keepass-gdrive /usr/local/bin
 cp configs/password-file.desktop /home/eric/.local/share/applications/
@@ -14,16 +17,17 @@ sudo dpkg --install google-chrome-*.deb && rm google-chrome-*.deb
  
 
 # command-line tools
-sudo apt-get install -y git gksu openssh-server
+sudo apt-get install -y git openssh-server curl
 google-chrome https://help.ubuntu.com/community/SSH/OpenSSH/Configuring
 
-# Sublime Text
-google-chrome https://www.sublimetext.com/
-read -p 'Install Sublime Text, then press any key to continue'
+# VSCode
+google-chrome https://code.visualstudio.com/download
 
 # rclone
 echo 'installing rclone'
 ./install-rclone
+sudo mkdir -p /etc/fish/completions
+sudo rclone genautocomplete fish
 
 # KeePass
 sudo apt-get install -y keepass2
@@ -38,8 +42,8 @@ sudo wget https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx
 sudo apt-get install xdotool
 
 # Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker $(whoami)
@@ -48,26 +52,25 @@ sudo usermod -aG docker $(whoami)
 google-chrome https://golang.org/doc/install
 
 # Fish
-sudo apt-add-repository ppa:fish-shell/release-2
+sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish
+chsh --shell /usr/bin/fish
+
+# Smartgit
+cd ~/Downloads
+wget https://www.syntevo.com/downloads/smartgit/smartgit-linux-20_2_4.tar.gz
+sudo tar -C /usr/local/ -xzf smartgit-linux*.tar.gz
+sudo ln -s /usr/local/smartgit/bin/smartgit.sh /usr/local/bin/smartgit
 
 # Misc software
-sudo apt-get install -y audacity inkscape gimp pinta terminator pdfshuffler
+sudo apt-get install audacity inkscape gimp pinta terminator pdfshuffler
 google-chrome http://www.syntevo.com/smartgit/download
-google-chrome http://wikis.olin.edu/linux/doku.php?id=nvidia
-google-chrome http://wikis.olin.edu/linux/doku.php?id=printing
 google-chrome http://askubuntu.com/questions/264280/how-to-remove-items-from-places-sidebar-in-nautilus-3-6
-google-chrome http://user.wolfram.com/
-google-chrome http://wiki.ros.org/ROS/Installation
-google-chrome https://www.jetbrains.com/go/download/#section=linux
-google-chrome https://www.jetbrains.com/pycharm/
 google-chrome https://download.visicut.org/
 google-chrome https://etcher.io/
 
 # Keybase
 curl -O https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
-sudo apt-get install -y -f
-
-# TODO: Matlab
+sudo apt-get install -f
